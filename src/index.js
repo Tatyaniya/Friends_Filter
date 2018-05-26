32ce766be75dcec9525cbcfafc9dbfa692e77ab2
Tatiana Melnychuk <afromyak13@gmail.com>
2018-05-26 23:19:41 +0300

    style import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import './src/css/style.css';
-const template = require('../index-content.hbs');
+import './style.css';
+require('../index.hbs');
 
 VK.init({
     apiId: 6490248
 });
+const save = document.getElementById('#save');
+
+save.addEventListener('click', () => {
+    save.style.backgroundColor = 'red';
+})
