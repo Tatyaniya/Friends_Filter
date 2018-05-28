3155d4270452d23ac361b737510b0840821186b3
Tatiana Melnychuk <afromyak13@gmail.com>
2018-05-28 17:04:50 +0300

    выводится список

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './style.css';
-require('../index.hbs');
+import templateFunc from '../friends.hbs';
+let template = require('../index.hbs');
 
 VK.init({
-    apiId: 6491942
+    apiId: 6491689
 });
 
 function auth() {
@@ -17,4 +18,30 @@ function auth() {
     });
 }
 
-auth().then(() => console.log('ok'));
+function callAPI(method, params) {
+    params.v = '5.76';
+
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, (data) => {
+            if (data.error) {
+                reject(data.error);
+            } else {
+                resolve(data.response);
+            }
+        });
+    })
+}
+
+    (async () => {
+        try {
+            await auth();
+            const [me] = await callAPI('users.get', { name_case: 'gen' });    
+            const friends = await callAPI('friends.get', { fields: 'city, country, photo_100' });
+            const html = templateFunc(friends);
+            const results = document.querySelector('#lists-left');
+    
+            results.innerHTML = html;
+        } catch (e) {
+            console.error(e);
+        }
+    })();
